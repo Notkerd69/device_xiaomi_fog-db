331f3b3aa5a279cf56fe4604d9a4cd922b54e53e
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-08-02 08:38:00 +0800

    fog: Add NotchBar Killer overlay
     * This overlay gets rid of the black bar in most apps/games, and lets the screen fill past the notch.
    
    Change-Id: Ic008ce5d569127d230dc44451929477e063c4471
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: isnrdra <isanoorendra@gmail.com>
    Signed-off-by: indraadp <indraproject777@gmail.com>

--- device.mk
+++ device.mk
@@ -341,7 +341,8 @@ PRODUCT_PACKAGES += \
     SettingsProviderFog \
     SystemUIFog \
     TelephonyFog \
-    WifiFog
+    WifiFog \
+    NotchBarKiller
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
