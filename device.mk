348fb496627056bb4a496915883050a35c0604dd
maytinhdibo <iammaytinhdibo@gmail.com>
2022-01-22 11:46:44 +0000

    sm6225-common: introduce battery-friendly-pocketmode
    DNM experimental

--- device.mk
+++ device.mk
@@ -343,6 +343,13 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pocketmode.xml
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
