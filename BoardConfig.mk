9d31128dced7a7d086c20cf2829a65d5c7724665
Divyanshu-Modi <divyan.m05@gmail.com>
2022-04-30 10:22:14 +0400

    fog: Disable kpti
    Upstream kernels have a kpti safe list and cortex a73 and cortex-a53 are in the list.
    Hence, are safe from meltdown and spectre attack So... disable kpti in favour of increased perf.
    
    link to kpti safe list:
    https://github.com/Divyanshu-Modi/linux-5.x/blob/v5.12/arch/arm64/kernel/cpufeature.c#L1379
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,7 +130,8 @@ BOARD_KERNEL_CMDLINE += \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
-    swiotlb=2048
+    swiotlb=2048 \
+    kpti=off
 
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 TARGET_FORCE_PREBUILT_KERNEL := true
