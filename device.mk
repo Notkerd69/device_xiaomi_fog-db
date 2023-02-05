5e0a4da702633b68e8717784f31048a2fcef473f
boedhack99 <boedhack@gmail.com>
2023-02-05 16:38:37 +0700

    fog: Switch to RRO Overlays
    Change-Id: I8905233112615ef6e06b3d14ca03b6f53d97dd8c

--- device.mk
+++ device.mk
@@ -338,11 +338,14 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_c3qn/com.nxp.mifare.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    CarrierConfigFog \
+    DialerFog \
+    FrameworksFog \
+    SettingsFog \
+    SettingsProviderFog \
+    SystemUIFog \
+    TelephonyFog
 
 # LibQti Perf
 PRODUCT_PACKAGES += \
