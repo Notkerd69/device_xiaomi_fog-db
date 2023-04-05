c7b15b0e8aed8218c69d70b911cea3288c9bdf1f
Notkeret <jerespn@gmail.com>
2023-04-05 11:13:37 -0400

    fog: cdrod now

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The xtendedOS Project
+# Copyright (C) 2022 The lineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_fog.mk
+    $(LOCAL_DIR)/lineage_fog.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_fog-user \
-    xtended_fog-userdebug \
-    xtended_fog-eng
+    lineage_fog-user \
+    lineage_fog-userdebug \
+    lineage_fog-eng
