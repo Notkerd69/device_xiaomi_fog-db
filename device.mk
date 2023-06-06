14fce6027200ac4adfd1f64e8af9cf32ce62b244
Agatha q <naufalmarjan2014@gmail.com>
2023-06-06 17:01:00 +0000

    fog: Adapth init.bengal.perf to specific device

--- device.mk
+++ device.mk
@@ -406,7 +406,7 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
-    init.bengal.perf.rc \
+    init.fog.perf.rc \
     init.target.rc \
     ueventd.qcom.rc
 
